2f4f15a92d6e6b9b287a0f76424c435e37b4329c
Anemaygi <gianemayumisan@gmail.com>
2024-09-28 02:37:45 +0800

    card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Example from "./components/Example";
+import OQueFazemos from "./components/OQueFazemos";
 
 export default function Home() {
   return (
-    <div className="bg-verdeLab grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Inicializando :D
-      <Example />
+    <div className="bg-escuroLab min-h-screen h-full">
+      <OQueFazemos />
     </div>
   );
 }
