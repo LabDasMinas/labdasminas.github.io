865794eba57427c8c92db4542a79036aa0d03914
Anemaygi <gianemayumisan@gmail.com>
2024-09-28 01:40:53 +0800

    style: add shadow

--- src/app/components/ComoColaborar.tsx
+++ src/app/components/ComoColaborar.tsx
@@ -29,15 +29,15 @@ export default function ComoColaborar() {
 
       <section className="flex flex-col gap-y-20 py-40 relative  bg-[#F1EFEF]">
         
-        <h2 className="font-bold text-4xl text-escuroLab text-center drop-shadow">Como colaborar?</h2>
+        <h2 className="font-bold text-4xl text-escuroLab text-center [text-shadow:_0_2px_4px_rgb(0_0_0_/_40%)]">Como colaborar?</h2>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-12 lg:gap-24 px-8 lg:px-24">
           {
             content.map((item, idx) => (
               <article className="flex gap-x-4 items-start justify-start">
-                <div><em className="text-[#F1EFEF] drop-shadow flex not-italic font-bold justify-center items-center text-center bg-roxoLab text-2xl rounded-full w-10 h-10 mt-1">{idx + 1}</em></div>
+                <div><em className="text-[#F1EFEF] flex not-italic font-bold justify-center items-center text-center bg-roxoLab text-2xl rounded-full w-10 h-10 mt-1 [text-shadow:_0_2px_8px_rgb(0_0_0_/_40%)]">{idx + 1}</em></div>
                 <div className="text-lg">
-                  <p className="font-semibold">
+                  <p className="font-semibold [text-shadow:_0_1px_3px_rgb(0_0_0_/_40%)]">
                     {item.title}
                   </p>
                   <p>
