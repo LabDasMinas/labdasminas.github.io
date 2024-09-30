7225f877e155d422dad26b0f71c7b9607f3bf8bc
Anemaygi <gianemayumisan@gmail.com>
2024-09-30 22:45:13 +0800

    feat: o q fazemos section

--- src/app/components/OQueFazemos.tsx
+++ src/app/components/OQueFazemos.tsx
@@ -1,35 +1,44 @@
 import Image from "@/components/ImageG";
 export default function OQueFazemos() {
     return (
+
         <section className="text-center text-white flex flex-col items-center justify-center relative">
-            <Image src="/greenblob-fazemos.svg" fill={true} alt="" className="object-cover object-top z-10 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]" />
-            
+            {/* <Image src="/greenblob-fazemos.svg" width={400} height={500} alt="" className=" absolute -left-48 -bottom-96 z-0 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]" /> */}
+
+            {/* <div className="bg-red-500 size-40 absolute -left-20 -bottom-20" /> */}
+
             <p className="text-xl mb-4">O que fazemos?</p>
             <h2 className="text-4xl text-verdeLab font-bold mb-8">O que fazemos?</h2>
             <p className="text-base">Saiba um pouco mais dos nossos principais objetivos e motivações</p>
 
-            <div className="grid grid-cols-3 gap-24 p-12 items-center justify-center w-[70%]">
+            <div className="flex flex-col lg:flex-row gap-12 p-12 items-center justify-center w-full">
 
-                <div className="bg-[#F4F4F4] h-80  w-full shadow-lg rounded-xl text-black border border-[#F3F3F3] gap-y-4 py-8 px-16 flex flex-col justify-center items-center">
+                <div className="bg-[#F4F4F4] w-full lg:w-72 shadow-lg rounded-xl text-black border border-[#F3F3F3] gap-y-4 py-8 px-16 flex flex-col justify-center items-center">
 
-                    <div className="bg-red-500 size-28 mb-8"></div>
-                    <h3 className="text-2xl font-semibold">Título</h3>
-                    <p className="text-sm text-ellipsis text-left text-ellipsis">Texto explicativo breve falando de objetivos bla bla bla bla aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-
-                </div>
-
-                <div className="bg-roxo-gradient h-96  w-full shadow-xl rounded-xl text-white border border-[#F3F3F3] gap-y-4 py-8 px-16 flex flex-col justify-center items-center">
-                    <div className="bg-red-500 size-28 mb-8"></div>
-                    <h3 className="text-2xl font-semibold">Título</h3>
-                    <p className="text-sm text-left text-ellipsis">Texto explicativo breve falando de objetivos bla bla bla bla aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaTexto explicativo breve falando de objetivos bla bla bla bla aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                    <div className="size-24 mb-8 relative">
+                        <Image src="/fazemos-1.png" fill={true} alt="" />
+                    </div>
+                    <h3 className="text-2xl font-semibold">Títualo</h3>
+                    <p className="text-sm break-words text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus urna sapien, convallis non accumsan luctus, luctus vulputate odio. Ut tincidunt, urna quis posuere pretium, nibh lectus pretium tellus, quis placerat orci nunc ac nulla.</p>
 
                 </div>
 
-                <div className="bg-[#F4F4F4] h-80 w-full shadow-lg rounded-xl text-black border border-[#F3F3F3] gap-y-4 py-8 px-16 flex flex-col justify-center items-center">
-
-                    <div className="bg-red-500 size-28 mb-8"></div>
+                <div className="bg-roxo-gradient  w-full lg:w-72 shadow-xl rounded-xl text-white border border-[#F3F3F3] gap-y-4 lg:py-20 py-8 px-16 flex flex-col justify-center items-center">
+                    <div className="size-24 mb-8 relative">
+                        <Image src="/fazemos-3.png" fill={true} alt="" />
+                    </div>
                     <h3 className="text-2xl font-semibold">Título</h3>
-                    <p className="text-sm text-left text-ellipsis">Texto explicativo breve falando de objetivos bla bla bla bla aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                    <p className="text-sm text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus urna sapien, convallis non accumsan luctus, luctus vulputate odio. Ut tincidunt, urna quis posuere pretium, nibh lectus pretium tellus, quis placerat orci nunc ac nulla.</p>
+
+                </div>
+
+                <div className="bg-[#F4F4F4] w-full lg:w-72 shadow-lg rounded-xl text-black border border-[#F3F3F3] gap-y-4 py-8 px-16 flex flex-col justify-center items-center">
+
+                    <div className="size-24 mb-8 relative">
+                        <Image src="/fazemos-2.png" fill={true} alt="" />
+                    </div>
+                    <h3 className="text-2xl font-semibold">Título</h3>
+                    <p className="text-sm text-left text-ellipsis">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus urna sapien, convallis non accumsan luctus, luctus vulputate odio. Ut tincidunt, urna quis posuere pretium, nibh lectus pretium tellus, quis placerat orci nunc ac nulla.</p>
 
                 </div>
 
@@ -37,5 +46,6 @@ export default function OQueFazemos() {
             </div>
 
         </section>
+
     );
 }
