e88977f3ded81e2f079aab3b297e979bf2d0d805
Anemaygi <gianemayumisan@gmail.com>
2024-09-30 22:59:20 +0800

    feat: o que fazemos section

--- src/app/components/OQueFazemos.tsx
+++ src/app/components/OQueFazemos.tsx
@@ -3,9 +3,9 @@ export default function OQueFazemos() {
     return (
 
         <section className="text-center text-white flex flex-col items-center justify-center relative">
-            {/* <Image src="/greenblob-fazemos.svg" width={400} height={500} alt="" className=" absolute -left-48 -bottom-96 z-0 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]" /> */}
 
-            {/* <div className="bg-red-500 size-40 absolute -left-20 -bottom-20" /> */}
+                <Image src="/greenblob-fazemos.svg" width={200} height={200} alt="" className=" absolute -left-12 -bottom-20 -z-10 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]" />
+
 
             <p className="text-xl mb-4">O que fazemos?</p>
             <h2 className="text-4xl text-verdeLab font-bold mb-8">O que fazemos?</h2>
@@ -13,7 +13,7 @@ export default function OQueFazemos() {
 
             <div className="flex flex-col lg:flex-row gap-12 p-12 items-center justify-center w-full">
 
-                <div className="bg-[#F4F4F4] w-full lg:w-72 shadow-lg rounded-xl text-black border border-[#F3F3F3] gap-y-4 py-8 px-16 flex flex-col justify-center items-center">
+                <div className="bg-[#F4F4F4] w-full  lg:w-72 xl:w-96 shadow-lg rounded-xl text-black border border-[#F3F3F3] gap-y-4 py-8 px-16 flex flex-col justify-center items-center">
 
                     <div className="size-24 mb-8 relative">
                         <Image src="/fazemos-1.png" fill={true} alt="" />
@@ -23,7 +23,7 @@ export default function OQueFazemos() {
 
                 </div>
 
-                <div className="bg-roxo-gradient  w-full lg:w-72 shadow-xl rounded-xl text-white border border-[#F3F3F3] gap-y-4 lg:py-20 py-8 px-16 flex flex-col justify-center items-center">
+                <div className="bg-roxo-gradient  w-full lg:w-72 xl:w-96 shadow-xl rounded-xl text-white border border-[#F3F3F3] gap-y-4 lg:py-20 py-8 px-16 flex flex-col justify-center items-center">
                     <div className="size-24 mb-8 relative">
                         <Image src="/fazemos-3.png" fill={true} alt="" />
                     </div>
@@ -32,7 +32,7 @@ export default function OQueFazemos() {
 
                 </div>
 
-                <div className="bg-[#F4F4F4] w-full lg:w-72 shadow-lg rounded-xl text-black border border-[#F3F3F3] gap-y-4 py-8 px-16 flex flex-col justify-center items-center">
+                <div className="bg-[#F4F4F4] w-full  lg:w-72 xl:w-96 shadow-lg rounded-xl text-black border border-[#F3F3F3] gap-y-4 py-8 px-16 flex flex-col justify-center items-center">
 
                     <div className="size-24 mb-8 relative">
                         <Image src="/fazemos-2.png" fill={true} alt="" />
