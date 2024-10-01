064cf4d226258fc23024896789825bbce9994e11
Stephani Henrique <stephani.henrique31@gmail.com>
2024-10-01 11:07:29 -0600

    correcao-hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -7,40 +7,35 @@ import Header from "@/components/Header"
 
 export default function Hero(){
     return (
-        <div>
-        <Header/>   
-        <section className="flex place-content-center 2xl:place-content-start md:min-h-[calc(100vh+150px)] 2xl:min-h-[calc(100vh+70px)] pb-32 md:pb-16" id="home">         
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 p-8 md:p-2 pt-24 md:pt-16 2xl:p-32">
+    <div>
+        <Header/>
+        <section className="flex flex-col w-full h-fit text-white px-8 md:p-20 relative mb-8">
+
+        <ImageG src="/header_green.svg" fill={true} alt="" className="object-cover object-bottom z-0 drop-shadow-[0_-4px_4px_rgba(0,0,0,0.25)]" />
+        <ImageG src="/header_pink.svg" fill={true} alt="" className="object-cover object-bottom z-1 -mt-16"/>
+
+        
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 p-8 pb-32 md:p-2 lg:p-16 2xl:pb-32 z-10">
                 <div className="2xl:ml-16">
-                    <div className="max-w-min flex flex-col justify-center min-w-[300px]  md:min-w-[400px]">   
+                    <div className="lg:max-w-min flex flex-col justify-center md:min-w-[400px]">   
                         <h1 className=" font-bold text-3xl sm:text-6xl max-w-max">
                             Lab das Minas
                         </h1>
-                         <p className="pt-5 md:pt-5 pb-5 md:pb-9 text-base leading-5 text-justify md:text-left">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                        <p className="pt-5 md:pt-5 pb-5 md:pb-9 text-base leading-5 text-justify md:text-left">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
                     </div>
 
                     <button className="bg-white text-rosaLab rounded-[30px] h-[50px] min-w-[180px] md:h-[60px] md:min-w-[200px] font-medium text-[18px]">
                         Saiba mais
                     </button>
                 </div>
-                
+                    
 
                 <div className="lg:col-span-3 w-full flex justify-center">
-                    <ImageG className="absolute right-0 transform -translate-y-[50%] md:-translate-y-[80%] 2xl:-translate-y-[60%] h-[210px] md:h-[620px] w-fit" src="/dinaFinal.png" height={200} width={500} alt=""/>
+                    <ImageG className="absolute right-0 transform -translate-y-[30%] lg:-translate-y-[80%] 2xl:-translate-y-[60%] h-[210px] lg:h-[550px] w-fit" src="/dinaFinal.png" height={200} width={500} alt=""/>
                 </div>
-                
-            </div>        
+                    
+            </div>
         </section>
-            <ImageG 
-                src="/header_green.svg" 
-                alt="" width={0} height={0}
-                className="absolute w-full h-[800px] object-fill top-0 md:h-fit -top-[50px] 2xl:-top-[120px] z-[-2]"
-            />
-            <ImageG 
-                src="/header_pink.svg" 
-                alt="" width={0} height={0}
-                className="absolute w-full h-[850px] object-fill md:h-fit -top-[50px] 2xl:-top-[120px] z-[-1]"
-            />
-        </div>
+    </div>
     );
 }                                                                                                                               
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { usePathname } from "next/navigation";
 import { useState } from "react";
 import ImageG from './ImageG';
 import { FiMenu } from "react-icons/fi";
@@ -15,7 +14,7 @@ export default function Header() {
 
     const [openMenu, setOpenMenu] = useState(false);
 
-    const handleScroll = (id) => {
+    const handleScroll = (id: string) => {
         const element = document.getElementById(id);
         if (element) {
             element.scrollIntoView({ behavior: 'smooth' });
@@ -24,7 +23,7 @@ export default function Header() {
     };
 
     return (
-        <header className="text-black py-4 px-8 lg:flex lg:justify-center lg:items-center relative bg-rosaLab md:bg-transparent">
+        <header className=" p-2 lg:py-0 text-black px-8 lg:flex lg:justify-center lg:items-center relative bg-rosaLab md:bg-transparent z-40">
             <div className="flex items-center">
                 <ImageG
                     src="/logo_Lab.png" 
@@ -36,18 +35,18 @@ export default function Header() {
                     {openMenu ? <FiX size={30} /> : <FiMenu size={30} />}
                 </button>
             </div>
-            <section className={`m-4 drop-shadow left-0 w-[calc(100vw-32px)] rounded-md z-40 p-4 lg:p-0 my-2 transition-all duration-300 max-lg:absolute max-lg:bg-verdeLab lg:flex lg:flex-grow lg:visible lg:opacity-100 ${openMenu ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
+            <section className={`m-4 drop-shadow left-0 w-[calc(100vw-32px)] rounded-md z-40 p-4 lg:p-0 my-2 transition-all duration-300 max-lg:absolute max-lg:bg-rosaLab lg:flex lg:flex-grow lg:visible lg:opacity-100 ${openMenu ? 'opacity-100 visible border border-[#B04B6B]' : 'opacity-0 invisible'}`}>
                 <nav className={`flex grow flex-col lg:flex-row lg:items-center lg:justify-end`}>
                     {navLinks.map((link) => (
                         <a 
                             key={link.title} 
                             onClick={() => handleScroll(link.id)}
-                            className={`transition-all lg:mx-2 xl:mx-8 text-white hover:font-bold cursor-pointer`}
+                            className={`transition-all lg:mx-2 xl:mx-8 text-white hover:font-medium cursor-pointer`}
                         >
                             {link.title}
                         </a>
                     ))}
-                    <button className='transition-all text-white' style={{ border: '1px solid white', borderRadius: 30, paddingInline: 20, paddingBlock: 5, marginLeft: '8px' }} onClick={() => handleScroll('colab')}>Colaborar</button>
+                    <button className='transition-all text-white mt-2 lg:mt-0 lg:ml-2' style={{ border: '1px solid white', borderRadius: 30, paddingInline: 20, paddingBlock: 5 }} onClick={() => handleScroll('colab')}>Colaborar</button>
                 </nav>
             </section>
         </header>
